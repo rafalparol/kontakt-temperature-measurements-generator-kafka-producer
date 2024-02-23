c614ffcd71e50bfec394a52613b8071cdd133cec
rafal-parol <rafal.parol@gmail.com>
2024-02-23 18:55:59 +0100

    First commit.

---
+++ src/main/java/inc/temp/right/always/temperaturemeasurementsgenerator/TemperatureMeasurementsGeneratorApplication.java
@@ -0,0 +1,13 @@
+package inc.temp.right.always.temperaturemeasurementsgenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemperatureMeasurementsGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemperatureMeasurementsGeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/inc/temp/right/always/temperaturemeasurementsgenerator/TemperatureMeasurementsGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package inc.temp.right.always.temperaturemeasurementsgenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemperatureMeasurementsGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
